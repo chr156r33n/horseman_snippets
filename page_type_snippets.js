760b9f5a520cd89885925e56d462809901b78cf1
Chris Green <75560394+chr156r33n@users.noreply.github.com>
2022-11-21 15:11:47 +0000

    Add files via upload

--- page_type_snippets.js
+++ page_type_snippets.js
@@ -1,57 +1,68 @@
-// Snippets for Page Type analysis
+// Evaluation Title
 
-// OG Page Type
+var title = document.title;
+
+switch (true) {
+   case title.includes('top '):
+   case title.includes('best '):
+   case title.includes('vs '):
+   case title.includes('verses '):return('evaulation')
+  break;
+  default:
+    return('nope');
+}
+
+// Is Big Ecom
+
+var host = window.location.host;
+
+switch (true) {
+   case host.includes('ebay'):
+   case host.includes('amazon'):
+  case host.includes('etsy'): return('big ecom')
+  break;
+  default:
+    return('nope');
+}
+
+// Is Social/Video
+
+var host = window.location.host;
+
+switch (true) {
+   case host.includes('facebook'):
+   case host.includes('twitter'):
+   case host.includes('instagram'):
+   case host.includes('pinterest'):return('social')
+  break;
+ case host.includes('tiktok'):
+  case host.includes('youtube'): return('video')
+
+  default:
+    return('nope');
+}
+
+// JSON Article Schema
+
+return [...document.querySelectorAll('script[type="application/ld+json"]')].filter(e => e.innerText.includes('"@type":"Article"')).length ? "present" : "not present"
+
+// JSON ListItem Schema
+
+return [...document.querySelectorAll('script[type="application/ld+json"]')].filter(e => e.innerText.includes('"@type":"ListItem"')).length ? "present" : "not present"
+
+// JSON Product Schema
+
+return [...document.querySelectorAll('script[type="application/ld+json"]')].filter(e => e.innerText.includes('"@type":"Product"')).length ? "present" : "not present"
+
+// OG Type
 
 return document.head.querySelector('[property="og:type"]').content
 
-/// Microdata Checks
+// Page Type From URL
 
-// Schema Microdata ListItem
-
-return document.querySelector('[itemtype="https://schema.org/ListItem"]')
-
-// Schema Microdata Product
-
-return document.querySelector('[itemtype="http://schema.org/Product"]');
-
-// Schema Microdata Article
-
-return document.querySelector('[itemtype="http://schema.org/Article"]');
-
-
-/// Page Title Checks
-
-// Page title contains "blog"
-
-var pagetitle = (document.title)
-var regex = /.*(B|b)(log).*/
-
-return regex.test(pagetitle);
-
-// Page title contains "guide"
-
-var pagetitle = (document.title)
-var regex = /.*(G|g)(uide).*/
-
-return regex.test(pagetitle);
-
-// Page title contains "How to"
-
-var pagetitle = (document.title)
-var regex = /.*((H|h)ow\s(T|to)).*/
-
-return regex.test(pagetitle);
-
-// URL Test snippet - 100% credit to https://github.com/jessthebp
-
-/* get pathname  */ 
 var docurl = document.URL;
 let thispath = new URL(docurl).pathname;
 let path = thispath.toLowerCase()
-
-//we have it in lower case bc "includes" is case sensitive
-// go through and set based on what you need baby
-// does anyone have a list of common url conventions for this kind of thing
 
 switch (true) {
    case path.includes('guide'):
@@ -60,9 +71,11 @@ switch (true) {
     break;
     case path.includes('cat'):
    case path.includes('plp'):
+   case path.includes('/collections/'):
+   case path.includes('/products/'):
   case path.includes('listing'): return('plp')
     break;
-        case path.includes('product'):
+    case path.includes('product'):
    case path.includes('pdp'):
   case path.includes('detail'): return('pdp')
     break;
@@ -71,3 +84,49 @@ switch (true) {
   default:
     return('unknown');
 }
+
+// PLP Item Count Present
+
+var regex = /[1-9]+[\d,.]*\s?(?:item|product|style)s?/ig
+return regex.test(document.body.innerText)
+
+// Question Mark In Title
+
+var title = document.title;
+
+switch (true) {
+   case title.includes('?'):return('question')
+  break;
+  default:
+    return('nothing');
+}
+
+// Question Terms in Title
+
+var title = document.title;
+
+switch (true) {
+   case title.includes('who'):
+   case title.includes('what'):
+   case title.includes('when'):
+   case title.includes('how'):
+  case title.includes('why'): return('question')
+  break;
+  default:
+    return('nope');
+}
+
+// Microformat Article
+
+var articleSchema = document.querySelector('[itemtype="http://schema.org/Article"]')
+if (articleSchema === null) {return('not present')} else {return('present')};
+
+// Microformat ListItem
+
+var listitem = document.querySelector('[itemtype="http://schema.org/ListItem"]')
+if (listitem === null) {return('not present')} else {return('present')};
+
+// Microformat Product
+
+var productSchema = document.querySelector('[itemtype="http://schema.org/Product"]')
+if (productSchema === null) {return('not present')} else {return('present')};
